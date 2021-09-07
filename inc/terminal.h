bcae597fb3236a74d601efd8959e35c3b53260d3
Ana Carolina <ancarolcs@gmail.com>
2021-09-07 16:56:25 -0300

    Add terminal module scratch

---
+++ inc/terminal.h
@@ -0,0 +1,8 @@
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+#include "temperature.h"
+
+void function();
+
+#endif
---
+++ src/terminal.c
@@ -0,0 +1,9 @@
+#include "terminal.h"
+
+void function()
+{
+    data_parameters params;
+    params.dt1 = 1;
+
+    set_user_mode(CONTROL_USER, params);
+}
