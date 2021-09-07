bcf04079fe3705ffc8339ccf681042f8c86b23e9
Ana Carolina <ancarolcs@gmail.com>
2021-09-07 11:11:41 -0300

    Add bme280 driver

---
+++ inc/bme280.h
@@ -0,0 +1,15 @@
+#ifndef __BME280__
+#define __BME280__
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+
+int bme280Init(int iChannel, int iAddr);
+int bme280ReadValues(int *T, int *P, int *H);
+
+#endif // __BME280__
