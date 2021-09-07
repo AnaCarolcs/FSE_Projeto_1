dca754dd661b30618574faad5604e11486c9da51
Ana Carolina <ancarolcs@gmail.com>
2021-09-07 11:05:46 -0300

    Add csv logger implementation

---
+++ src/log.c
@@ -0,0 +1,33 @@
+#include "log.h"
+
+void create_csv()
+{
+    FILE *file = fopen("log.csv", "w");
+
+    fprintf(file, "DATETIME, INTERNA, EXTERNA, REFERENCIA, CONTROLE\n");
+
+    fclose(file);
+}
+
+const char *get_time_info()
+{
+    static char timestamp[20];
+    time_t mytime;
+
+    mytime = time(NULL);
+    struct tm tm = *localtime(&mytime);
+
+    sprintf(timestamp, "%02d-%02d-%02d-%02d:%02d:%02d", tm.tm_mday, tm.tm_mon + 1,
+            tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
+
+    return timestamp;
+}
+
+void write_on_log(temperature_info temp, int controle)
+{
+    FILE *file = fopen("log.csv", "a");
+
+    fprintf(file, "%s,%f,%f,%f,%d\n", getTimestamp(), temp.interna,
+            temp.externa, temp.referencia, controle);
+    fclose(file);
+}
