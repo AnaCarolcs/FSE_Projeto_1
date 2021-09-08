17b4117f7498ad5b4e318bbcdbec5d691a7cde49
Ana Carolina <ancarolcs@gmail.com>
2021-09-08 03:24:19 -0300

    Add threads creation and functions to run in parallel

--- src/main.c
+++ src/main.c
@@ -1,20 +1,39 @@
+#include <pthread.h>
 #include <stdio.h>
 
 #include "control.h"
+#include "terminal.h"
 
 #include "bme280.h"
 #include "log.h"
 #include "pwm.h"
 
-int main()
+pthread_t CONTROL_THREAD, TERMINAL_THREAD;
+
+void start()
 {
     bme280Init(1, 0x76);
     create_csv();
     init_pwm();
 
-    control();
+    pthread_create(&CONTROL_THREAD, NULL, control, NULL);
+    pthread_create(&TERMINAL_THREAD, NULL, read_user_option, NULL);
+    pthread_join(CONTROL_THREAD, NULL);
+    pthread_join(TERMINAL_THREAD, NULL);
+}
 
+void quit()
+{
     end_pwm();
+
+    pthread_cancel(TERMINAL_THREAD);
+    pthread_cancel(CONTROL_THREAD);
+}
+
+int main()
+{
+
+    start();
 
     return 0;
 }
